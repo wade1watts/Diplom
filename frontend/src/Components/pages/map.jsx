cd2088396abc50e4428e9896a1a4d237411fc2cd
Wade Watts <ne0ser1yozno@yandex.ru>
2023-04-12 12:05:11 +0300

    Добавлено модальное окно

--- frontend/src/Components/pages/map.jsx
+++ frontend/src/Components/pages/map.jsx
@@ -5,6 +5,7 @@ import L from "leaflet";
 import '../Map.css'
 import PostService from "../../API/GETIinfo";
 import MarkerClusterGroup from "@changey/react-leaflet-markercluster";
+import Modal from "../Modal/Modal";
 
 const markerIcon = new L.Icon({
     iconUrl: require("./images/pin.png"),
@@ -14,6 +15,8 @@ const markerIcon = new L.Icon({
 })
 
 function Map() {
+
+    const [modalActive, setModalActive] = useState(false)
 
     const [info, setinfo] = useState([])
 
@@ -26,22 +29,34 @@ function Map() {
 
             return(
                 <div>
+
+                   
+
                     {/* <Sidebar/> */}
                 <MapContainer center={[55.74,  37.61]} zoom={8} zoomControl={false} >
-                
+                <Modal active={modalActive} setActive={setModalActive}><h1>sdfghjk</h1></Modal>
                     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                     <ZoomControl position='bottomright' />
                     <MarkerClusterGroup>
+                    
                     { info.map((pars) =>
                         
                             <Marker position={[pars.latitude, pars.longitude]} icon={markerIcon}>
+                                 
                                 <Popup>
-                                {[pars.city]} <br/> {[pars.latitude]} <br/> {[pars.longitude]} <br/> <button onClick={alert}>Изменить</button>
+                                {[pars.city]} <br/> {[pars.latitude]} <br/> {[pars.longitude]} <br/> 
+                                <button onClick={() => setModalActive(true)}>Открыть</button>
                                 </Popup>
-                            </Marker>                        
+                                 
+                            </Marker>                                                 
                     )}
+
                     </MarkerClusterGroup>
+                    
                 </MapContainer>
+                <Modal active={modalActive} setActive={setModalActive}><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Velit voluptatum sunt et quibusdam molestias qui, quas cumque quo, laboriosam ipsum officiis quisquam porro fugiat,
+                     modi nulla ad veniam minima accusantium.</p></Modal>
                 </div>
             )
 }
